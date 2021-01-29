7618a225e5e6dcd3747c0b6f2e4bfcb54951d6fa
lumf <luminfeng@ergengtv.com>
2021-01-29 16:45:20 +0800

    配置脚本支持 gateway docker镜像

---
+++ ruoyi-gateway/docker_build.sh
@@ -0,0 +1,5 @@
+#mvn clean package -Dmaven.test.skip=true -f pom.xml
+#version=$(date +%Y%m%d%H%M)
+
+docker build -f Dockerfile -t sunriver/ruoyi-cloud:gateway-1.0.0.0-test .
+docker push sunriver/ruoyi-cloud:gateway-1.0.0.0-test
